d3bbe881db61a1126b3797ec9ca2fc21930c6177
Amoshe, Richard (k21057746) <richard.amoshe@kcl.ac.uk>
2022-10-03 17:15:06 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,14 @@
+
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range(0, len(items)):
+                   keyName = str(items[i])
+                   frequencies[keyName] = items[i]
     return frequencies
+
+pytest.main("test_sample.py --capture=sys")
+
