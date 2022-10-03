47fccba25fe2d47bd5f920c87d68479f8f996d95
Amoshe, Richard (k21057746) <richard.amoshe@kcl.ac.uk>
2022-10-03 17:24:32 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -5,8 +5,12 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for i in range(0, len(items)):
-                   keyName = str(items[i])
-                   frequencies[keyName] = items[i]
+        keyName = str(items[i])
+        freqeuncy = 0
+        for i in range(0, len(items)):
+            if str(items[i]) == keyName:
+                frequency += 1
+        frequencies[keyName] = frequency
     return frequencies
 
 
