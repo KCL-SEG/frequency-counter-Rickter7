73de9920518f4800073b037d51f7d567cc4ab68c
Amoshe, Richard (k21057746) <richard.amoshe@kcl.ac.uk>
2022-10-04 10:43:55 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -9,9 +9,4 @@ def frequencies(items):
     length = len(frequencies)
     for i in range(0, length):
         keyName = str(frequencies[i])
-        for i in range(0, length):
-            if str(items[i]) == keyName:
-                frequency += 1
-        frequencies[keyName] = frequency
-        frequency = 0
     return frequencies
